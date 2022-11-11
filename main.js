feb6216bd14fec35ba276ea85bc57aad52f6e269
George Andersen <g.andersen@hotmail.co.uk>
2022-11-11 13:56:28 +0000

    Use fix for: Cannot read properties of undefined (reading 'default')

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ client.initialize();
 client.on("qr", (qr) => qrcode.generate(qr, { small: true }));
 client.on("ready", () => console.log("Client is ready!"));
 
-setTimeout(() => {
-  console.log(!!client.pupPage);
-  client.pupPage.on("response", async (response) => {
-    const stuff = [response.url(), response.status(), await response.text()];
-    console.log(
-      ...stuff.map((thing) =>
-        thing.length > 300 ? `${thing.slice(0, 30)}...` : thing
-      )
-    );
-  });
-}, 2000);
+// setTimeout(() => {
+//   console.log(!!client.pupPage);
+//   client.pupPage.on("response", async (response) => {
+//     const stuff = [response.url(), response.status(), await response.text()];
+//     console.log(
+//       ...stuff.map((thing) =>
+//         thing.length > 300 ? `${thing.slice(0, 30)}...` : thing
+//       )
+//     );
+//   });
+// }, 2000);
 
 let currentFn = undefined;
 
